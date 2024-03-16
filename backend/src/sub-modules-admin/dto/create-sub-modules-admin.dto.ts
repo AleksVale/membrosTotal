65c3877a2c4fcff3016da69509b4ce30c6d863ed
aleksander.vale <aleksander.vale@sofex.com.br>
2024-03-16 00:38:38 -0300

    craete DTO for the creates

--- backend/src/sub-modules-admin/dto/create-sub-modules-admin.dto.ts
+++ backend/src/sub-modules-admin/dto/create-sub-modules-admin.dto.ts
@@ -1,1 +1,20 @@
-export class CreateSubModulesAdminDto {}
+import { createZodDto } from 'nestjs-zod';
+import { z } from 'nestjs-zod/z';
+
+// create a Zod schema for Module creation
+const createSubModuleSchema = z.object({
+  title: z
+    .string({ required_error: 'O título é obrigatório' })
+    .min(3, { message: 'Título deve ter no mínimo 3 caracteres' }),
+  description: z
+    .string({ required_error: 'A descrição é obrigatório' })
+    .min(3, { message: 'A descrição deve ter no mínimo 3 caracteres' }),
+  moduleId: z.number({ required_error: 'O id do treinamento é obrigatório' }),
+});
+
+// create a DTO class using createZodDto
+export class CreateSubModuleAdminDTO extends createZodDto(
+  createSubModuleSchema,
+) {
+  // you can add additional properties or methods if needed
+}

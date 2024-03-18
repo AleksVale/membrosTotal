bc6f9ed91397177e7caa5686da2bfdc539a15f36
aleksander.vale <aleksander.vale@sofex.com.br>
2024-03-18 07:06:42 -0300

    subindo build

--- frontend/src/pages/collaborator/Trainings/list.tsx
+++ frontend/src/pages/collaborator/Trainings/list.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import useListTraining from './hooks/useListTraining'
+// import useListTraining from './hooks/useListTraining'
 import CarouselTrainingItem from '@/components/CarouselTrainingItem'
 import {
   Carousel,

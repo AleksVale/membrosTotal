b7aca0381a769d5b0ba6e7f33146b0393c013616
AleksVale <aleksander.vale@sofex.com.br>
2024-04-06 14:12:50 -0300

    update build

--- frontend/src/pages/admin/Training/new.tsx
+++ frontend/src/pages/admin/Training/new.tsx
@@ -1,6 +1,7 @@
 import { useCreateTraining } from './hooks/useCreateTraining'
 import { CreateEditTrainingForm } from './components/CreateEditTrainingForm'
 import { BaseHeader } from '@/components/BaseHeader'
+import { Helmet } from 'react-helmet-async'
 
 export function CreateTraining() {
   const { form, handleSubmitForm, isSubmitting } = useCreateTraining()

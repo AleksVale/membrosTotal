78eb2a2d0fc3b43c9be1b0140803ae5a80a51226
AleksVale <alexalexx3@gmail.com>
2024-04-11 02:30:52 -0300

    teste2

--- frontend/src/lib/http.ts
+++ frontend/src/lib/http.ts
@@ -12,7 +12,7 @@ interface ErrorResponse {
 // Create a custom Axios instance
 // const serverURL = 'http://localhost:5005/api'
 // const serverURL = 'https://membros-api.onrender.com/api'
-const serverURL = 'api'
+const serverURL = '/api'
 
 const http: AxiosInstance = axios.create({
   baseURL: serverURL,
